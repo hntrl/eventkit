aec90bd1f42c8624a3f0e56eb6459b55b57da39d
hntrl <hunter@hntrl.io>
2025-02-24 15:38:35 -0700

    allow asyncobserver to return any

--- packages/async-observable/lib/types.ts
+++ packages/async-observable/lib/types.ts
@@ -42,7 +42,7 @@ export interface Unsubscribable {
 }
 
 export interface AsyncObserver<T> {
-  (value: T): PromiseOrValue<void>;
+  (value: T): PromiseOrValue<any>;
 }
 
 export interface SubscriptionLike extends Unsubscribable {
