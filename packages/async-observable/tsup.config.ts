87c1513c7171fef9e7dad9b321bb31e036d95e0a
hntrl <hunter@hntrl.io>
2025-03-10 00:58:05 -0600

    chore: fix build scripts

--- packages/async-observable/tsup.config.ts
+++ packages/async-observable/tsup.config.ts
@@ -1,23 +1,17 @@
-import { defineConfig, Format } from "tsup";
+import { defineConfig } from "tsup";
 import { createBanner } from "../../scripts/utils";
 
 import pkg from "./package.json";
 
-const config = (format: Format) =>
-  defineConfig([
-    {
-      clean: false,
-      entry: ["lib/index.ts"],
-      format,
-      dts: true,
-      banner: {
-        js: createBanner(pkg.name, pkg.version),
-      },
-    },
-  ]);
-
 export default defineConfig([
-  // @ts-expect-error
-  ...config("esm"),
-  ...config("cjs"),
+  {
+    clean: true,
+    entry: ["lib/index.ts"],
+    format: ["cjs", "esm"],
+    outDir: "dist",
+    dts: true,
+    banner: {
+      js: createBanner(pkg.name, pkg.version),
+    },
+  },
 ]);
