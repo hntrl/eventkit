6ce90a79696955245f8fb1bf43367a7de232f411
hntrl <hunter@hntrl.io>
2025-03-22 16:49:10 -0600

    fix: duplicated tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,21 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    workspace: [
-      "packages/*",
-      {
-        test: {
-          exclude: [
-            "packages/_*/**",
-            "**/node_modules/**",
-            "**/dist/**",
-            "**/cypress/**",
-            "**/.{idea,git,cache,output,temp}/**",
-            "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*",
-          ],
-          include: ["**/*.{test,spec}.?(c|m)[jt]s?(x)", "**/__tests__/**/*.?(c|m)[jt]s?(x)"],
-        },
-      },
-    ],
+    workspace: ["packages/[^_]*"],
   },
 });
