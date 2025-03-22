e7c346c69f2ccad0d4dd4c3a3d401bbf2f10ef68
hntrl <hunter@hntrl.io>
2025-03-21 22:27:45 -0600

    fix(async-observable): readable streams never getting released

--- packages/async-observable/lib/from.ts
+++ packages/async-observable/lib/from.ts
@@ -1,4 +1,5 @@
 import { AsyncObservable } from "./observable";
+import { kCancelSignal } from "./subscriber";
 import {
   type InteropAsyncObservable,
   type AsyncObservableInput,
@@ -186,22 +187,19 @@ function fromAsyncIterable<T>(asyncIterable: AsyncIterable<T>) {
 }
 
 function fromReadableStreamLike<T>(readableStream: ReadableStreamLike<T>) {
-  return fromAsyncIterable(readableStreamLikeToAsyncGenerator(readableStream));
-}
-
-export async function* readableStreamLikeToAsyncGenerator<T>(
-  readableStream: ReadableStreamLike<T>
-): AsyncGenerator<T> {
-  const reader = readableStream.getReader();
-  try {
-    while (true) {
-      const { value, done } = await reader.read();
-      if (done) {
-        return;
+  return new AsyncObservable<T>(async function* (sub) {
+    const reader = readableStream.getReader();
+    try {
+      while (true) {
+        const { value, done } = await Promise.race([
+          reader.read(),
+          sub[kCancelSignal].then(() => ({ value: undefined, done: true as const })),
+        ]);
+        if (done) return;
+        yield value!;
       }
-      yield value!;
+    } finally {
+      reader.releaseLock();
     }
-  } finally {
-    reader.releaseLock();
-  }
+  });
 }
