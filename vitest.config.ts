5f741f5c9cfc78c4f334e4b40cd9a10b9afb87b7
hntrl <hunter@hntrl.io>
2025-03-21 22:28:40 -0600

    fix: include test files

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
             "**/.{idea,git,cache,output,temp}/**",
             "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*",
           ],
-          include: [],
+          include: ["**/*.{test,spec}.?(c|m)[jt]s?(x)", "**/__tests__/**/*.?(c|m)[jt]s?(x)"],
         },
       },
     ],
