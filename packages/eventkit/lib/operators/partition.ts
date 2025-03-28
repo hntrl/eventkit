f7cf86188f5267121e932e703b7554a2e832c15e
hntrl <hunter@hntrl.io>
2025-03-28 17:07:33 -0600

    feat(eventkit): smarter types for partition operator

--- packages/eventkit/lib/operators/partition.ts
+++ packages/eventkit/lib/operators/partition.ts
@@ -1,7 +1,13 @@
 import { type AsyncObservable, type UnaryFunction } from "@eventkit/async-observable";
 
 import { not } from "../utils/operators";
+import { type TruthyTypesOf } from "../utils/types";
 import { filter } from "./filter";
+
+type PartitionOperatorFunction<T, L = T, R = Exclude<T, L>> = UnaryFunction<
+  AsyncObservable<T>,
+  [AsyncObservable<L>, AsyncObservable<R>]
+>;
 
 /**
  * Returns an array with two observables that act as a split of the source observable; one with
@@ -12,12 +18,20 @@ import { filter } from "./filter";
  * the value is emitted on the second observable in the array. The `index` parameter is the number
  * `i` for the i-th source emission that has happened since the subscription, starting from 0.
  */
-export function partiton<T>(
+export function partition<T, S extends T>(
+  predicate: (value: T, index: number) => value is S
+): PartitionOperatorFunction<T, S>;
+export function partition<T>(
   predicate: (value: T, index: number) => boolean
-): UnaryFunction<AsyncObservable<T>, [AsyncObservable<T>, AsyncObservable<T>]> {
-  return (source: AsyncObservable<T>) =>
-    [source.pipe(filter(predicate)), source.pipe(filter(not(predicate)))] as [
-      AsyncObservable<T>,
-      AsyncObservable<T>,
-    ];
+): PartitionOperatorFunction<T>;
+export function partition<T>(
+  predicate: BooleanConstructor
+): PartitionOperatorFunction<T, TruthyTypesOf<T>>;
+export function partition<T>(
+  predicate: ((value: T, index: number) => boolean) | BooleanConstructor
+): PartitionOperatorFunction<T, any, any> {
+  return (source: AsyncObservable<T>) => [
+    source.pipe(filter(predicate)) as AsyncObservable<any>,
+    source.pipe(filter(not(predicate))) as AsyncObservable<any>,
+  ];
 }
