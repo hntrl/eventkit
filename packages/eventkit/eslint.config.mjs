642ff743d16e71cf785207101b7304a69c552f89
hntrl <hunter@hntrl.io>
2025-03-27 23:49:33 -0600

    fix(eventkit): add timing globals

--- packages/eventkit/eslint.config.mjs
+++ packages/eventkit/eslint.config.mjs
@@ -5,12 +5,16 @@ import shared from "../../eslint.config.mjs";
 
 // @ts-check
 
+const restrictedNodeGlobals = Object.keys(globals.node).filter(
+  (key) => !["setTimeout", "setInterval"].includes(key)
+);
+
 export default tseslint.config(...shared, {
   languageOptions: {
     globals: globals.commonjs,
   },
   rules: {
-    "no-restricted-globals": ["error", ...Object.keys(globals.node)],
+    "no-restricted-globals": ["error", ...restrictedNodeGlobals],
     "@typescript-eslint/no-explicit-any": "off",
     "import/no-nodejs-modules": "error",
     "import/named": "off",
