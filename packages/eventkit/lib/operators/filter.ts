5437459536a948e1320f7d36d658692c9f248826
hntrl <hunter@hntrl.io>
2025-03-28 17:07:58 -0600

    fix(eventkit): reorganize overloads

--- packages/eventkit/lib/operators/filter.ts
+++ packages/eventkit/lib/operators/filter.ts
@@ -9,10 +9,10 @@ import { type TruthyTypesOf } from "../utils/types";
  * @param predicate A function that evaluates each value emitted by the source Observable.
  * Returns true to keep the value, false to drop it.
  */
-export function filter<T>(predicate?: (value: T, index: number) => boolean): OperatorFunction<T, T>;
 export function filter<T, S extends T>(
   predicate?: (value: T, index: number) => value is S
 ): OperatorFunction<T, S>;
+export function filter<T>(predicate?: (value: T, index: number) => boolean): OperatorFunction<T, T>;
 export function filter<T>(predicate?: BooleanConstructor): OperatorFunction<T, TruthyTypesOf<T>>;
 export function filter<T>(
   predicate?: ((value: T, index: number) => boolean) | BooleanConstructor
